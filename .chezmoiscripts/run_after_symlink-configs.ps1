59322a0df4eb09b94d3922d90c4cb01daa800a70
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-07-31 21:06:10 +0600

    remove symlinking script as it doesn't work

--- .chezmoiscripts/run_after_symlink-configs.ps1
+++
@@ -1,9 +0,0 @@
-If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
-  If (-Not (Test-Path $env:LOCALAPPDATA\nvim)) {
-    New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config/nvim
-  }
-
-  If (-Not (Test-Path $env:APPDATA\nushell)) {
-    New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config/nushell
-  }
-}
